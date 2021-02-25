aa8e48deb6aab902dc21794815fc57e779ffaa00
btphill4 <btphill4@asu.edu>
2021-02-24 19:08:59 -0800

    Updated

--- Code/project1/Code/heap.h
+++ Code/project1/Code/heap.h
@@ -14,31 +14,44 @@
 
 // ELEMENT is  a  data  type  that  contains  a  field  namedkey,  
 // which  is  of  typeint.   Note  that ELEMENT should not be of typeint
-class Element
+typedef struct TAG_ELEMENT
 {
-    int key; 
-};
+    int key;
+}ELEMENT;
 
-class ELEMENT *ElementT;
 
+typedef ELEMENT *ElementT;  //points to an ELEMENT struct called ElementT
 
 // HEAP is a data type that contains three fields named capacity(of typeint),
 // size(of typeint),and H(an array of type 
 // ELEMENT with index ranging from 0 to capacity)
-class HEAP
+typedef struct TAG_HEAP
 {
-    int capacity;
-    int size;
-    ELEMENT *H;
+    
+    int capacity; /* max size of the heap */
+    int size; /* current size of the heap */
+    ElementT *H; /* pointer to pointers to elements (**Element)*/
+    int n;
+
+    //methods//
+    ELEMENT* initalize(int n);  
+    void heapPrint(HEAP *H);
+    //void percUp(HEAP *H, int pos);
+    //void percDown(HEAP *H, int pos);
+    //int getSize();
+
+}HEAP;
 
     
-    void heapInit(int capacity);
-    void heapPrint(HEAP *H);
-    void percUp(HEAP *H, int pos);
-    void percDown(HEAP *H, int pos);
-    //int size();
-};
+/** 
+ typedef struct {
+  int n;
+  boolean success;
+  char s[16];
+} my_struct_t; 
 
+  my_struct_t myStruct = {10, true, "Hello!"}; 
+  my_struct_t * ptr;*/
 
 
 /**
