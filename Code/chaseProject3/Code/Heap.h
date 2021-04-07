815a5ecaec4a0304e9c05513e81b29e0527fe120
btphill4 <btphill4@asu.edu>
2021-04-06 19:44:25 -0700

    Final commit for the day

---
+++ Code/chaseProject3/Code/Heap.h
@@ -0,0 +1,17 @@
+#ifndef HEAP_HPP
+# define HEAP_HPP
+
+class Heap
+{
+	public:
+		/*Heap(void);
+		Heap(** replace parameters **);
+		Heap(Heap const &instance);
+		Heap &operator=(Heap const &rhs);
+		~Heap(void);*/
+
+	private:
+
+};
+
+#endif
